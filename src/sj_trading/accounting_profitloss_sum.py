62d70f1c9ee2f810d1f2d735254ea4bb5ba1a4d7
Howard Huang <blackhuang0121@gmail.com>
2025-07-25 14:31:22 +0800

    Create scripts for accounting ex-dividend-amt

--- src/sj_trading/accounting_profitloss_sum.py
+++ src/sj_trading/accounting_profitloss_sum.py
@@ -6,13 +6,64 @@ import gspread
 
 def profitloss_sum(year=2025):
     api = get_api() 
-    # 已實現損益總計
+    # 1. 取得 已實現損益總計
     profitloss_sum = api.list_profit_loss_summary(api.stock_account, f'{year}-01-01', f'{year}-12-31')
     total_dict = profitloss_sum.total.__dict__
     df_total = pd.DataFrame([total_dict])
+
     df_total['year'] = year  # 加上年度欄位
 
-    print(df_total)
+    # 2. 取得已實現損益中的現金股利
+    profitloss = api.list_profit_loss(api.stock_account, f'{year}-01-01', f'{year}-12-31')
+    all_details = []
+    for p in profitloss:
+        detail_id = p.id
+        details = api.list_profit_loss_detail(api.stock_account, detail_id)
+        all_details.extend(details)
+    df_detail = pd.DataFrame(p.__dict__ for p in all_details)
+    total_dividend = df_detail['ex_dividend_amt'].sum() if not df_detail.empty else 0
+
+    df_total['ex_dividend_amt'] = total_dividend   # 新增到同一 row
+
+    # 3. 計算不含息報酬率
+    # 純資本利得 = pnl（總損益）- total_dividend
+    # 報酬率 = 純資本利得 / buy_cost
+    pure_trade_profit = df_total['pnl'][0] - total_dividend
+    pr_ratio_pure = pure_trade_profit / df_total['buy_cost'][0] * 100 if df_total['buy_cost'][0] != 0 else 0  # 百分比
+    df_total['pr_ratio_pure'] = round(pr_ratio_pure, 2)
+
+      # 4. 新增三個欄位
+    df_total['year'] = year
+    df_total['ex_dividend_amt'] = total_dividend
+    df_total['pr_ratio_pure'] = round(pr_ratio_pure, 2)
+
+    
+    # 5. 欄位順序
+    # 省略 entry_amount、cover_amount
+    final_cols = [
+        'year', 'quantity', 'buy_cost', 'sell_cost', 'pnl', 'ex_dividend_amt', 'pr_ratio', 'pr_ratio_pure'
+    ]
+    # 若欄位不足自動補齊0
+    for col in final_cols:
+        if col not in df_total.columns:
+            df_total[col] = 0
+    df_total = df_total[final_cols]
+
+
+    # 再重新命名
+    col_map = {
+        'year': '年度',
+        'quantity': '成交數量',
+        'buy_cost': '買進金額',
+        'sell_cost': '賣出金額',
+        'pnl': '損益',
+        'ex_dividend_amt': '已實現股利',
+        'pr_ratio': '含息報酬率',
+        'pr_ratio_pure': '不含息報酬率'
+    }
+    df_total = df_total.rename(columns=col_map)
+
+    # print(df_total)
 
     # 寫入 Google Sheet
     scope = [
@@ -25,16 +76,6 @@ def profitloss_sum(year=2025):
     client = gspread.authorize(creds)
     spreadsheet = client.open_by_key('1VENNdDiBglXuO1bTlGp3Fl-G1Z9c_GRLgRGdEAWo6D8')
     sheet = spreadsheet.worksheet('api_profitloss_sum')
-
-    # # 先判斷是否已經有標題，沒有則加上
-    # records = sheet.get_all_records()
-    # if not records:
-    #     sheet.append_row(df_total.columns.values.tolist())
-
-    # # append 一行新資料
-    # sheet.append_row(df_total.values.tolist()[0])
-    # print(f'已新增 {year} 年度已實現損益總計到 Google Sheet')
-    # return df_total
 
 # 讀取所有現有資料
     records = sheet.get_all_records()
